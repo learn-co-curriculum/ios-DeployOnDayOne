b18366ef6c3860aa1b804fc7c9906b4d15d7f352
magfurulabeer <magfurulabeer@gmail.com>
2016-01-12 11:44:58 -0500

    Add persistence and developer menu

--- deployOnDayOne/MyApp.m
+++ deployOnDayOne/MyApp.m
@@ -21,9 +21,21 @@
 {
     self = [super init];
     if (self) {
-        self.database = [[NSMutableDictionary alloc] init];
+        NSDictionary *savedDatabase = [[NSUserDefaults standardUserDefaults] dictionaryForKey:@"studentDatabase"];
+        if ( savedDatabase ) {
+            self.database = [savedDatabase mutableCopy];
+        } else {
+            self.database = [[NSMutableDictionary alloc] init];
+        }
+        
+        NSArray *savedQuestions = [[NSUserDefaults standardUserDefaults] arrayForKey:@"interviewQuestions"];
+        if (savedQuestions) {
+            self.interviewQuestions = [savedQuestions mutableCopy];
+        } else {
+            self.interviewQuestions = [[NSMutableArray alloc] init];
+        }
+        
         self.currentUser = nil;
-        self.interviewQuestions = [[NSMutableArray alloc] init];
     }
     return self;
 }
@@ -78,6 +90,9 @@
             case 3:
                 [self readInterviewsMenu];
                 break;
+            case 0:
+                [self developerWarning];
+                break;
             default:
                 NSLog(@"Error. Please enter a number from 1-3.");
                 [self mainMenu];
@@ -85,6 +100,96 @@
         }
     }
     
+}
+
+#pragma mark - Developer Methods 
+
+-(void)developerWarning{
+    NSLog(@"You are attempting to enter developer mode. Are you sure?");
+    NSString *input = [[self requestKeyboardInput] uppercaseString];
+    if ([input isEqualToString:@"YES"]) {
+        [self developerMode];
+    } else if ([input isEqualToString:@"NO"]) {
+        [self mainMenu];
+    } else {
+        NSLog(@"Error. Please type in YES or NO. Input is not case sensitive.");
+        [self developerWarning];
+    }
+}
+
+-(void)developerMode {
+    NSLog(@"You are in Developer Mode.");
+    NSLog(@"Enter a command");
+    NSArray *commands = @[ @"EXIT", @"READ", @"HELP", @"CLEAR DATABASE", @"CLEAR QUESTIONS", @"CLEAR ALL" ];
+    NSString *input = [[self requestKeyboardInput] uppercaseString];
+    if ( [commands containsObject:input] ) {
+        switch ( [commands indexOfObject:input] ) {
+            case 0:
+                [self developerExit];
+                break;
+            case 1:
+                [self developerRead];
+                break;
+            case 2:
+                [self developerHelp];
+                break;
+            case 3:
+            case 4:
+            case 5:
+                [self developerClear:input];
+                break;
+            default:
+                break;
+        }
+    } else {
+        NSLog(@"Invalid Command");
+        NSLog(@"Enter HELP for a list of commands");
+        [self developerMode];
+    }
+}
+
+-(void)developerExit {
+    NSLog(@"You are now exiting Developer Mode");
+    [self mainMenu];
+}
+
+-(void)developerClear:(NSString *)command {
+    if ([command isEqualToString:@"CLEAR ALL"]) {
+        [[NSUserDefaults standardUserDefaults] removeObjectForKey:@"studentDatabase"];
+        self.database = [@{} mutableCopy];
+        [[NSUserDefaults standardUserDefaults] removeObjectForKey:@"interviewQuestions"];
+        self.interviewQuestions = [@[] mutableCopy];
+    } else if ([command isEqualToString:@"CLEAR DATABASE"]) {
+        [[NSUserDefaults standardUserDefaults] removeObjectForKey:@"studentDatabase"];
+        self.database = [@{} mutableCopy];
+    } else if ([command isEqualToString:@"CLEAR QUESTIONS"]) {
+        [[NSUserDefaults standardUserDefaults] removeObjectForKey:@"interviewQuestions"];
+        self.interviewQuestions = [@[] mutableCopy];
+    }
+    NSLog(@"Cleared!");
+    [self developerMode];
+}
+
+-(void)developerRead {
+    if (self.database == nil) {
+        NSLog(@"Database is empty");
+    } else {
+        for (NSString *student in [self.database allKeys] ) {
+            NSLog(@"%@:", student);
+            [self printStudentQA:self.database[student]];
+        }
+        [self developerMode];
+    }
+}
+
+-(void)developerHelp {
+    NSArray *commands = @[ @"EXIT", @"CLEAR DATABASE / CLEAR QUESTIONS / CLEAR ALL", @"READ", @"HELP" ];
+
+    NSLog(@"List of possible Commands:");
+    for (NSUInteger i = 0; i < [commands count]; i++) {
+        NSLog(@"%lu. %@", i+1, commands[i]);
+    }
+    [self developerMode];
 }
 
 #pragma mark - Be Interviewed Methods
@@ -141,13 +246,13 @@
         NSLog(@"There are currently no questions to be asked.");
         NSLog(@"Would you want to write one instead?");
         NSLog(@"Simply type in YES or NO, and press enter.");
-        NSString *input = [self requestKeyboardInput];
+        NSString *input = [[self requestKeyboardInput] uppercaseString];
         if ([input isEqualToString:@"YES"]) {
             [self newInterviewQuestionMenu];
         } else if ([input isEqualToString:@"NO"]) {
             [self mainMenu];
         } else {
-            NSLog(@"Error. Please type in YES or NO. Input is case sensitive.");
+            NSLog(@"Error. Please type in YES or NO. Input is not case sensitive.");
             [self randomQuestion];
         }
     } else {
@@ -181,7 +286,7 @@
     [self addAnswer:answer toQuestion:chosenQuestion forStudent:self.currentUser];
     
     NSLog(@"Would you like to answer another question?");
-    NSString *yesOrNO = [self requestKeyboardInput];
+    NSString *yesOrNO = [[self requestKeyboardInput] uppercaseString];
     
     if ([yesOrNO isEqualToString:@"YES"]) {
         if (random) {
@@ -192,7 +297,7 @@
     } else if ([yesOrNO isEqualToString:@"NO"]) {
         [self mainMenu];
     } else {
-        NSLog(@"Error. Please type in YES or NO. Input is case sensitive.");
+        NSLog(@"Error. Please type in YES or NO. Input is case insensitive.");
     }
 }
 
@@ -205,21 +310,21 @@
     if ([self.interviewQuestions containsObject:newQuestion]) {
         NSLog(@"Question is already part of database.");
         NSLog(@"Do you still want to add a question?");
-        NSString *yesOrNO = [self requestKeyboardInput];
+        NSString *yesOrNO = [[self requestKeyboardInput] uppercaseString];
         
         if ([yesOrNO isEqualToString:@"YES"]) {
             [self newInterviewQuestionMenu];
         } else if ([yesOrNO isEqualToString:@"NO"]) {
             [self mainMenu];
         } else {
-            NSLog(@"Error. Please type in YES or NO. Input is case sensitive.");
+            NSLog(@"Error. Please type in YES or NO. Input is case insensitive.");
             [self newInterviewQuestionMenu];
         }
     } else {
         [self addQuestion:newQuestion];
         NSLog(@"Question Added!");
         NSLog(@"Would you like to add another question?");
-        NSString *yesOrNO = [self requestKeyboardInput];
+        NSString *yesOrNO = [[self requestKeyboardInput] uppercaseString];
         
         if ([yesOrNO isEqualToString:@"YES"]) {
             [self newInterviewQuestionMenu];
@@ -252,53 +357,59 @@
             NSLog(@"This student has had no interviews yet");
             NSLog(@"Go back to main menu?");
             
-            NSString *yesOrNO = [self requestKeyboardInput];
+            NSString *yesOrNO = [[self requestKeyboardInput] uppercaseString];
             
             if ([yesOrNO isEqualToString:@"YES"]) {
                 [self mainMenu];
             } else if ([yesOrNO isEqualToString:@"NO"]) {
                 [self readInterviewsMenu];
             } else {
-                NSLog(@"Error. Please type in YES or NO. Input is case sensitive.");
+                NSLog(@"Error. Please type in YES or NO. Input is case insensitive.");
                 [self readInterviewsMenu];
             }
         } else {
-            NSArray *keys = [chosenStudent allKeys];
-            for (NSString *question in keys) {
-                NSString *answer = chosenStudent[question];
-                NSLog(@"Q: %@", question);
-                NSLog(@"A: %@", answer);
-                NSLog(@"-----------------------------------");
-            }
+            [self printStudentQA:chosenStudent];
             NSLog(@"Would you like to read someone elses interview?");
-            NSString *yesOrNO = [self requestKeyboardInput];
+            NSString *yesOrNO = [[self requestKeyboardInput] uppercaseString];
             
             if ([yesOrNO isEqualToString:@"YES"]) {
                 [self readInterviewsMenu];
             } else if ([yesOrNO isEqualToString:@"NO"]) {
                 [self mainMenu];
             } else {
-                NSLog(@"Error. Please type in YES or NO. Input is case sensitive.");
+                NSLog(@"Error. Please type in YES or NO. Input is case insensitive.");
             }
         }
 
     }
 }
 
+-(void)printStudentQA:(NSDictionary *)chosenStudent {
+    NSArray *keys = [chosenStudent allKeys];
+    for (NSString *question in keys) {
+        NSString *answer = chosenStudent[question];
+        NSLog(@"Q: %@", question);
+        NSLog(@"A: %@", answer);
+        NSLog(@"-----------------------------------");
+    }
+}
 
 
 #pragma mark - Database Methods
 
 -(void)addStudent:(NSString *)student {
     self.database[student] = [[NSMutableDictionary alloc] init];
+    [[NSUserDefaults standardUserDefaults] setObject:self.database forKey:@"studentDatabase"];
 }
 
 -(void)addQuestion:(NSString *)question{
     [self.interviewQuestions addObject:question];
+    [[NSUserDefaults standardUserDefaults] setObject:self.interviewQuestions forKey:@"interviewQuestions"];
 }
 
 -(void)addAnswer:(NSString *)answer toQuestion:(NSString *)question forStudent:(NSString *)student {
     self.database[student][question] = answer;
+    [[NSUserDefaults standardUserDefaults] setObject:self.database forKey:@"studentDatabase"];
 }
 
 
